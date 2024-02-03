25699ad458f7f3c2056463347f15949d7220d11d
Antonis Zikas <antoniszks2003@gmail.com>
2024-02-04 00:29:31 +0200

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-from Source.app import App
-
-if __name__ == "__main__":
-    myApp = App() # Create an App object
-    myApp.run()   # Call the run() method of the App object to start the application
